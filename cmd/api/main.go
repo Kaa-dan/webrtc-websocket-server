8b865a24250fb48fc5753694add62050e99e93c4
Kaa-dan <nithinraj.dev@gmail.com>
2025-07-30 01:50:17 +0530

    added production level logger

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,14 @@
 package main
 
-func main(){
+import "github.com/Kaa-dan/webrtc-websocket-server.git/internal/config"
 
+func main() {
 
-	
+	cfg := config.MustLoad()
+
+	//logging config (excluding sensitive values)
+	if cfg.LogLevel == "debug" || cfg.Environment == "development" {
+		cfg.PrintConfig()
+	}
+
 }
