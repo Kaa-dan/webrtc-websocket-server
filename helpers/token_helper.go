f7b084bfdfde2203fd2292a61c83c4bb356566de
Kaa-dan <nithinraj.dev@gmail.com>
2025-07-31 00:18:37 +0530

    validateToken method created

--- helpers/token_helper.go
+++ helpers/token_helper.go
@@ -1,8 +1,10 @@
 package helpers
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/Kaa-dan/webrtc-websocket-server.git/database"
 	"github.com/golang-jwt/jwt/v4"
@@ -10,12 +12,12 @@ import (
 )
 
 type SignedDetail struct {
-	Email     string
-	FirstName string
-	Last_name string
-	Uid       string
-	User_type string
-	jwt.Claims
+	Email      string
+	First_name string
+	Last_name  string
+	Uid        string
+	User_type  string
+	jwt.RegisteredClaims
 }
 
 type TokenManager struct {
@@ -23,6 +25,7 @@ type TokenManager struct {
 	secretKey      string
 }
 
+// creates new TokenManager instance
 func NewTokenManager() *TokenManager {
 
 	secretKey := os.Getenv("SECRET_KEY")
@@ -33,4 +36,77 @@ func NewTokenManager() *TokenManager {
 		userCollection: database.GetCollection("users"),
 		secretKey:      secretKey,
 	}
+}
+
+// GenerateAllTokens  : generates both access and refresh token
+func (tm *TokenManager) GeneratAllToken(email, firstName, lastName, userType, uid string) (string, string, error) {
+	// Create claims for access token (expires in 24 hours)
+	claims := &SignedDetail{
+		Email:      email,
+		First_name: firstName,
+		Last_name:  lastName,
+		Uid:        uid,
+		User_type:  userType,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * 24)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			NotBefore: jwt.NewNumericDate(time.Now()),
+		},
+	}
+	// Create claims for refresh token (expires in 168 hours = 7 days)
+
+	refreshClaims := &SignedDetail{
+		Email:      email,
+		First_name: firstName,
+		Last_name:  lastName,
+		Uid:        uid,
+		User_type:  userType,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * 168)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			NotBefore: jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	// Generate access token
+	token, err := jwt.NewWithClaims(jwt.SigningMethodES256, claims).SignedString([]byte(tm.secretKey))
+
+	if err != nil {
+		return "", "", fmt.Errorf("failed to generate access token: %w", err)
+	}
+
+	// Generate refresh token
+	refreshToken, err := jwt.NewWithClaims(jwt.SigningMethodHS256, refreshClaims).SignedString([]byte(tm.secretKey))
+	if err != nil {
+		return "", "", fmt.Errorf("failed to generate refresh token: %w", err)
+	}
+	return token, refreshToken, nil
+}
+
+// ValidateToken validate the JWT  token and returns claims
+
+func (tm *TokenManager) ValidateToken(signedToken string) (*SignedDetail, error) {
+	token, err := jwt.ParseWithClaims(
+		signedToken,
+		&SignedDetail{},
+		func(t *jwt.Token) (interface{}, error) {
+			if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+			}
+			return []byte(tm.secretKey), nil
+		},
+	)
+	if err != nil {
+		return nil, fmt.Errorf("invalid token: %w", err)
+	}
+
+	claims, ok := token.Claims.(*SignedDetail)
+	if !ok {
+		return nil, fmt.Errorf("invalid token claims")
+	}
+	if claims.ExpiresAt.Before(time.Now()) {
+		return nil, fmt.Errorf("token is expired")
+	}
+
+	return claims, nil
 }
