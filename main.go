5f9be2ec3eaee2afbafcde15ff82d927cafd41e9
Kaa-dan <nithinraj.dev@gmail.com>
2025-07-31 03:54:38 +0530

    user-signup mode

--- main.go
+++ main.go
@@ -24,11 +24,13 @@ func init() {
 func main() {
 	// router setup
 	router := gin.Default()
-	//auth
+	//auth route setup
 	tokenHelper := helpers.NewTokenHelper()
 	authManager := managers.NewAuthManager(tokenHelper)
 	authHandler := handlers.NewAuthHandler(authManager)
-
+	//registering auth-routes
 	authHandler.RegisterAuthApis(router)
-	router.Run()
+
+	//starting server
+	router.Run(":8000")
 }
